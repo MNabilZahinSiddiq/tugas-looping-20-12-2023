c56759641782ed29df9effa140ed475ed9390a03
MNabilZahinSiddiq <m.nabilzahinsiddiq@smkwikrama.sch.id>
2023-12-20 13:39:09 +0700

    tugas looping 20-12-2023

---
+++ script.js
@@ -0,0 +1,23 @@
+// Tugas 1 start
+for (let number = 1 ; number <= 50 ; number++) {
+    console.log(number)
+}
+
+let number1 = 1
+while(number1 <= 50) {
+    console.log(number1);
+    number1++
+}
+// Tugas 1 end
+
+// Tugas 2 start
+for (let angka = 50 ; angka >= 1 ; angka--) {
+    console.log(angka)
+}
+
+let angka1 = 50
+while(angka1 >= 1) {
+    console.log(angka1);
+    angka1--
+}
+// Tugas 2 start
